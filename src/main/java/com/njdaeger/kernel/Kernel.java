e2a8a365a5c4cf6d98b9b92cc21d29f4ada7454f
NJDaeger <noahdaeger@hotmail.com>
2017-05-16 07:32:09 -0400

    Added Enable and disable methods.

--- src/main/java/com/njdaeger/kernel/Kernel.java
+++ src/main/java/com/njdaeger/kernel/Kernel.java
@@ -3,4 +3,14 @@ package com.njdaeger.kernel;
 import com.coalesce.plugin.CoPlugin;
 
 public class Kernel extends CoPlugin {
+	
+	@Override
+	public void onPluginEnable() throws Exception {
+	
+	}
+	
+	@Override
+	public void onPluginDisable() throws Exception {
+	
+	}
 }
